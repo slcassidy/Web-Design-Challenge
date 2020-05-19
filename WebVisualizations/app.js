be9da832a14afda03b61cb749ee582609897da45
slcassidy <samantha.l.cassidy@gmail.com>
2020-05-19 00:30:11 -0400

    added linkage between pages

--- WebVisualizations/app.js
+++ WebVisualizations/app.js
@@ -1,27 +1,32 @@
+$("#ALL").show();   
 $(document).ready(function(){
-    $("#ALL").show();    
+ 
     $("#Max").click(function(){
       $("#Fig1").show();
       $("#Fig2").hide();
       $("#Fig3").hide();
       $("#Fig4").hide();
+      $("#ALL").show();
     });
     $("#Hum").click(function(){
       $("#Fig1").hide();
       $("#Fig2").show();
       $("#Fig3").hide();
       $("#Fig4").hide();
+      $("#ALL").show();
     });
     $("#Clo").click(function(){
         $("#Fig1").hide();
         $("#Fig2").hide();
         $("#Fig3").show();
         $("#Fig4").hide();
+        $("#ALL").show();
       });
     $("#Win").click(function(){
         $("#Fig1").hide();
         $("#Fig2").hide();
         $("#Fig3").hide();
         $("#Fig4").show();
+        $("#ALL").show();
     });
   });
